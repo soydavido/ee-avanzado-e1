b7016e5c61969e74ccdf60fa16a72021270be394
soydavido <ortuod@gmail.com>
2024-10-01 23:24:14 -0400

    Primer Commit

---
+++ index.js
@@ -0,0 +1,7 @@
+function clickBotonContador(){
+    alert('Alerta!!');
+}
+
+
+//Aqui, deben hacer una funcion, que debera imprimir los numeros del 1 al 20 por consola
+//Esta debe llamarse desde el boton 2
